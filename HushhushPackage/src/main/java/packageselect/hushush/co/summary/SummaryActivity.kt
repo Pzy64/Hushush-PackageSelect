6bf3bf03e8c0807f5097e98c8123497d1ecedc70
Prafuldev V R <thepzy64@gmail.com>
2018-10-29 13:12:46 +0530

    Image loaded in summary

--- HushhushPackage/src/main/java/packageselect/hushush/co/summary/SummaryActivity.kt
+++ HushhushPackage/src/main/java/packageselect/hushush/co/summary/SummaryActivity.kt
@@ -3,9 +3,14 @@ package packageselect.hushush.co.summary
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import packageselect.hushush.co.R
 
 import kotlinx.android.synthetic.main.summary_activity.*
+import kotlinx.android.synthetic.main.summary_content.*
+import java.io.File
 
 class SummaryActivity : AppCompatActivity() {
 
@@ -13,6 +18,17 @@ class SummaryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.summary_activity)
         setSupportActionBar(toolbar)
+        toolbar.title = "Preview"
+
+
+        Glide.with(this)
+                .load(File(externalCacheDir.absolutePath + "/image.jpg"))
+                .apply(RequestOptions()
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .skipMemoryCache(true)
+                )
+                .into(preview)
+
 
     }
 
